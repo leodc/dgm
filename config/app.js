59cc7e8f29277e333261105bc0b3126195c54145
Leonel Castañeda <leodcastaneda@gmail.com>
2017-12-01 10:12:03 -0600

    config files

---
+++ config/app.js
@@ -0,0 +1,6 @@
+module.exports  = {
+    encryption_key      : 'ERTETERTERTERTRETERT',
+    page_size           : '10000',
+    uploads_path        : '/upload',
+    uploads_tmp_path    : '/upload'
+};
---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports  = {
+    'database'  : 'dgm_test',
+    'host'      : '198.199.118.197',
+    'port'      : '27017'
+};
